4bbedecfd0506c2b59d91b1a735fa55ff5642f23
iMADi-ARCH <nandanaditya985@gmail.com>
2021-03-17 15:16:36 +0530

    Recreated the site

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,21 @@
 
+// // let heading = document.getElementById("main-heading");
 
-let menu_items = document.getElementsByClassName("menu-item");
-for (i = 0; i < menu_items.length; i++) {
-    // console.log(item);
-    if (menu_items[i].getAttribute("href") == window.location.href.split('/').pop()) {
-        menu_items[i].style.width = "7em";
-        menu_items[i].style.background = "#264653";
-    }
-}
+// // heading.style.height = screen.availHeight + "px";
 
-console.log();
+// // console.log();
+
+// // detecting android
+// if(screen.width < 540) {
+//     let grid = document.getElementsByClassName("grid-container");
+//     // console.log(contents);
+//     for (i = 0; i < grid.length; i++) {
+//         grid[i].style.display = "inline";
+//     }
+
+//     let headings = document.getElementsByClassName("heading");
+//     // console.log(contents);
+//     for (i = 0; i < headings.length; i++) {
+//         headings[i].style.fontSize = "2.5em";
+//     }
+// }
